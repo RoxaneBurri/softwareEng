94e2fe7c9e62c0011462ea101f5da437caa6a9f4
RoxaneBurri <roxane.burri@etu.unige.ch>
2020-10-07 23:54:41 +0200

    git ignore

---
+++ app/src/main/java/com/github/RoxaneBurri/bootcamp/weather/WeatherService.java
@@ -0,0 +1,22 @@
+package com.github.RoxaneBurri.bootcamp.weather;
+
+import java.io.IOException;
+
+import com.github.RoxaneBurri.bootcamp.location.Location;
+
+/**
+ * This interface represents a service that enables you to get weather forecasts for a specific
+ * location.
+ */
+public interface WeatherService {
+
+    /**
+     * Get the weather forecast at a given location.
+     *
+     * @param location the location for which you want to get the forecast
+     * @return the weather forecast for the given location
+     * @throws IOException if there is a network error of any kind
+     */
+    WeatherForecast getForecast(Location location) throws IOException;
+
+}
